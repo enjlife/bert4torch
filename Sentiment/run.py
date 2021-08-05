5770b94ef4a8f2d87d2fefe93246dc70b9488c34
haonan7 <haonan7@staff.weibo.com>
2021-08-05 18:55:43 +0800

    add sentiment

--- Sentiment/run.py
+++ Sentiment/run.py
@@ -1,27 +1,34 @@
 # coding: UTF-8
 import time
 import torch
-from train_eval import train
-from bert_pytorch import BertConfig, BertForSequenceClassification, BertTokenizer
+from train_eval import train, test
+from bert_pytorch import BertConfig, BertForMaskedLM, BertTokenizer
 from importlib import import_module
 import argparse
 from utils import build_dataset, build_iterator, get_time_dif
 
 
 parser = argparse.ArgumentParser(description='Chinese Text Classification')
-# parser.add_argument('--model', type=str, required=True, help='choose a model: Bert, ERNIE')
+parser.add_argument('--model_path', type=str, default='../pretrained_model/bert-base-chinese', help='pretrained model path')
+parser.add_argument('--train_path', type=str, default='D:\\sentiment_data\\sentiment\\train')
+parser.add_argument('--dev_path', type=str, default='D:\\sentiment_data\\sentiment\\dev')
+parser.add_argument('--test_path', type=str, default='D:\\sentiment_data\\sentiment\\test')
+parser.add_argument('--class_path', type=str, default='D:\\sentiment_data\\sentiment\\class.txt')
+parser.add_argument('--mode', type=str, default='test', help='train or test')
+
 args = parser.parse_args()
 
 
 class Config(object):
     """配置训练参数"""
-    def __init__(self, dataset):
+    def __init__(self, args):
         self.model_name = 'bert'
-        self.train_path = '../data/' + dataset + '/train.txt'
-        self.dev_path = '../data/' + dataset + '/dev.txt'
-        self.test_path = '../data/' + dataset + '/test.txt'
-        self.class_list = [x.strip() for x in open('../data/' + dataset + 'class.txt').readlines()]  # 类别名单
-        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.mode = args.mode
+        self.train_path = args.train_path
+        self.dev_path = args.dev_path
+        self.test_path = args.test_path
+        self.class_list = [x.strip() for x in open(args.class_path).readlines()]  # 类别名单
+        self.save_path = '.trained_model' + '/' + self.model_name + '.ckpt'        # 模型训练结果
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
         self.require_improvement = 1000                                 # early stopping: 1000 batches
         self.num_classes = len(self.class_list)                         # 类别数: label_num
@@ -49,13 +56,15 @@ if __name__ == '__main__':
     start_time = time.time()
     print("Loading data...")
     train_data, dev_data, test_data = build_dataset(config)
-    train_iter = build_iterator(train_data, config)
-    dev_iter = build_iterator(dev_data, config)
+    if args.mode == 'train':
+        train_iter = build_iterator(train_data, config)
+        dev_iter = build_iterator(dev_data, config)
     test_iter = build_iterator(test_data, config)
     time_dif = get_time_dif(start_time)
     print("Time usage:", time_dif)
-
+    model = BertForMaskedLM.from_pretrained(config.model_path).to(config.device)
     # train
-    model = BertForSequenceClassification.from_pretrained(config.model_path, config.num_classes).to(config.device)
-    # next(bert.parameters()).is_cuda  -> check if model is on cuda
-    train(config, model, train_iter, dev_iter, test_iter)
+    if args.mode == 'train':
+        train(config, model, train_iter, dev_iter, test_iter)
+    else:
+        test(config, model, test_iter)
---
+++ Sentiment/utils.py
@@ -0,0 +1,111 @@
+# coding: UTF-8
+import os
+import torch
+from tqdm import tqdm
+import time
+import random
+from datetime import timedelta
+
+PAD, CLS, MASK = '[PAD]', '[CLS]', '[MASK]'  # padding符号, bert中综合信息符号
+
+
+def build_dataset(config):
+
+    def load_dataset(path, max_len=128):
+        prefix = '很满意。'
+        mask_idx = 1
+        mask_id = config.tokenizer.vocab[MASK]
+        pos_id = config.tokenizer.vocab['很']
+        neg_id = config.tokenizer.vocab['不']
+        contents = []
+        with open(path, 'r', encoding='UTF-8') as f:
+            for line in tqdm(f):
+                lin = line.strip()
+                if not lin:
+                    continue
+                content, label = lin.split('\t')
+                content = prefix + content
+                token = config.tokenizer.tokenize(content)
+                token = [CLS] + token
+                token[mask_idx] = mask_id  # 'CLS MASK ...'
+                seq_len = len(token)
+                mask = []
+                token_ids = config.tokenizer.convert_tokens_to_ids(token)
+
+                if max_len:
+                    if len(token) < max_len:
+                        mask = [1] * len(token_ids) + [0] * (max_len - len(token))
+                        token_ids += ([0] * (max_len - len(token)))
+                    else:
+                        mask = [1] * max_len
+                        token_ids = token_ids[:max_len]
+                        seq_len = max_len
+                contents.append((token_ids, int(label), seq_len, mask))
+        return contents
+    train, dev = None, None
+    test = load_dataset(config.test_path, config.pad_size)
+    if config.mode == 'train':
+        train = load_dataset(config.train_path, config.pad_size)
+        dev = load_dataset(config.dev_path, config.pad_size)
+    return train, dev, test
+
+
+class DatasetIterater(object):
+    def __init__(self, data_list, batch_size, device):
+        self.batch_size = batch_size
+        self.data_list = data_list
+        self.n_batches = len(data_list) // batch_size
+        self.residue = False  # 记录batch数量是否为整数
+        if len(data_list) % self.n_batches != 0:
+            self.residue = True
+        self.index = 0
+        self.device = device
+
+    def _to_tensor(self, datas):
+        x = torch.LongTensor([data[0] for data in datas]).to(self.device)
+        y = torch.LongTensor([data[1] for data in datas]).to(self.device)
+        # pad前的长度(超过pad_size的设为pad_size)
+        seq_len = torch.LongTensor([data[2] for data in datas]).to(self.device)
+        mask = torch.LongTensor([data[3] for data in datas]).to(self.device)
+        return (x, seq_len, mask), y
+
+    def __next__(self):
+        if self.residue and self.index == self.n_batches:
+            batches = self.data_list[self.index * self.batch_size: len(self.data_list)]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+        elif self.index >= self.n_batches:
+            self.index = 0
+            raise StopIteration
+        else:
+            batches = self.data_list[self.index * self.batch_size: (self.index + 1) * self.batch_size]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        if self.residue:
+            return self.n_batches + 1
+        else:
+            return self.n_batches
+
+
+def build_iterator(dataset, config):
+    iter = DatasetIterater(dataset, config.batch_size, config.device)
+    return iter
+
+
+def get_time_dif(start_time):
+    """获取已使用时间"""
+    end_time = time.time()
+    time_dif = end_time - start_time
+    return timedelta(seconds=int(round(time_dif)))
+
+
+if __name__ == '__main__':
+    pass
