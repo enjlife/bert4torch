99f26e01b250002ad4ffb54a6827625860405d5b
haonan7 <haonan7@staff.weibo.com>
2021-09-24 20:43:22 +0800

    crf

--- CRF/cnn_word_seg.py
+++ CRF/cnn_word_seg.py
@@ -81,8 +81,7 @@ model.summary()
 
 model.compile(loss=crf.loss,  # 用crf自带的loss
               optimizer='adam',
-              metrics=[crf.accuracy]  # 用crf自带的accuracy
-             )
+              metrics=[crf.accuracy])  # 用crf自带的accuracy
 
 
 def max_in_dict(d):  # 定义一个求字典中最大值的函数
--- CRF/cnn_word_seg_torch.py
+++ CRF/cnn_word_seg_torch.py
@@ -6,29 +6,29 @@ import re
 import random
 
 
-
 class CnnWordSeg(nn.Module):
     """CNN 分词"""
     def __init__(self, num_labels, vocab_size, hidden_size):
         super(CnnWordSeg, self).__init__()
         self.embedding = nn.Embedding(vocab_size, hidden_size, padding_idx=0)
         self.conv1 = torch.nn.Sequential(
-            torch.nn.Conv1d(in_channels=10, out_channels=16, kernel_size=3, stride=1, padding=1, padding_mode='replicate'),
+            # 这里采用重复填充 padding=1填充一层
+            torch.nn.Conv1d(in_channels=128, out_channels=128, kernel_size=3, stride=1, padding=1, padding_mode='replicate'),
             torch.nn.ReLU()
         )
         self.conv2 = torch.nn.Sequential(
-            torch.nn.Conv1d(10, 16, 3, 1, 1, padding_mode='replicate'),
+            torch.nn.Conv1d(128, 128, 3, 1, 1, padding_mode='replicate'),
             torch.nn.ReLU()
         )
         self.conv3 = torch.nn.Sequential(
-            torch.nn.Conv1d(10, 16, 3, 1, 1, padding_mode='replicate'),
+            torch.nn.Conv1d(128, 128, 3, 1, 1, padding_mode='replicate'),
             torch.nn.ReLU()
         )
         self.dense = nn.Linear(hidden_size, 4)
-        self.crf = CRF(num_tags=num_labels, batch_first=False)
+        self.crf = CRF(num_tags=num_labels, batch_first=True)
 
     def forward(self, x, y, mask):
-        hidden_state = self.embedding(x)
+        hidden_state = self.embedding(x)  # (batch,seq_len,hidden_size)
 
         hidden_state = self.conv1(hidden_state)
         hidden_state = self.conv2(hidden_state)
@@ -39,15 +39,81 @@ class CnnWordSeg(nn.Module):
         return hidden_state
 
 
-def build_dataset():
-    sents = open('../icwb2-data/training/msr_training.utf8').read().strip().split('\r\n')
+class DatasetIterater(object):
+    def __init__(self, data_list, batch_size, device):
+        self.batch_size = batch_size
+        self.data_list = data_list
+        self.n_batches = len(data_list) // batch_size
+        self.residue = False  # 记录batch数量是否为整数
+        if len(data_list) % self.n_batches != 0:
+            self.residue = True
+        self.index = 0
+        self.device = device
+
+    def _to_tensor(self, datas):
+        token_ids = torch.LongTensor([data[0] for data in datas]).to(self.device)
+        y = torch.LongTensor([data[1] for data in datas]).to(self.device)
+        token_type_ids = torch.LongTensor([data[2] for data in datas]).to(self.device)
+        mask = torch.LongTensor([data[3] for data in datas]).to(self.device)
+        return (token_ids, token_type_ids, mask), y
+
+    def __next__(self):
+        if self.residue and self.index == self.n_batches:
+            batches = self.data_list[self.index * self.batch_size: len(self.data_list)]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+        elif self.index >= self.n_batches:
+            self.index = 0
+            raise StopIteration
+        else:
+            batches = self.data_list[self.index * self.batch_size: (self.index + 1) * self.batch_size]
+            self.index += 1
+            batches = self._to_tensor(batches)
+            return batches
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        if self.residue:
+            return self.n_batches + 1
+        else:
+            return self.n_batches
+
+
+def build_dataset(path):
+    sents = open(path, 'r').read().strip().split('\r\n')
     sents = [re.split(' +', s) for s in sents]  # 词之间以两个空格隔开
     sents = [[w for w in s if w] for s in sents]  # 去掉空字符串
     random.shuffle(sents)  # 打乱语料，以便后面划分验证集
-    chars, id2char, char2id = build_vocab(sents)
+    id2char, char2id = build_vocab(sents)
+    trains, valids = sents[:-5000], sents[-5000:]
 
 
-def build_vocab(sents, min_count=5):
+def to_id(data, char2id):
+    datasets = []
+    for s in data:
+        x, y = [], []
+        for w in s:
+            if not all(c in char2id for c in w):
+                continue
+            x.extend([char2id[c] for c in w])
+            if len(w) == 1:
+                y.append(0)
+            elif len(w) == 2:
+                y.extend([1, 3])
+            else:
+                y.append()
+
+
+def build_iterator(dataset, config):
+    iter = DatasetIterater(dataset, config.batch_size, config.device)
+    return iter
+
+
+def build_vocab(sents, min_count=2):
     chars = {}
     for s in sents:
         for c in ''.join(s):
@@ -58,10 +124,11 @@ def build_vocab(sents, min_count=5):
     chars = {i: j for i, j in chars.items() if j >= min_count}
     id2char = {i: j for i, j in enumerate(chars.keys())}
     char2id = {j: i for i, j in id2char.items()}
-    return chars, id2char, char2id
+    return id2char, char2id
 
 
 if __name__ == '__main__':
+    path = '../icwb2-data/training/msr_training.utf8'
     model = CnnWordSeg(4, 128, 10000)
     x = list(model.named_parameters())
     print(model.named_parameters())
